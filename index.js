e158cfaffb32948a18e36274e970aa1bfccf3ab6
Olivia <oliviaflexx@gmail.com>
2021-08-23 14:27:11 -0400

    fade in js

--- index.js
+++ index.js
@@ -2,14 +2,53 @@
 function nav() {
     let icon = document.getElementById('icon')
     let nav = document.getElementById('nav')
-    nav_list = nav.classList
-    console.log(nav_list)
-    if (nav_list.contains('active')) {
+    navList = nav.classList
+    if (navList.contains('active')) {
         icon.classList.remove('rotate_animation');
         nav.classList.remove('active');
     } else {
         icon.classList.add('rotate_animation');
         nav.classList.add('active');
     }
-    
 }
+
+// Intersection observer //
+/* $(window).on( "load", function() { 
+    const places = document.querySelectorAll('.fade-in');
+
+    observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('appear');
+            }
+        });
+    });
+
+    places.forEach(image => {
+    observer.observe(image);
+    });
+}) */
+
+window.addEventListener("load", (event) => {
+    const places = document.querySelectorAll('.fade-in');
+
+    observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.add('appear');
+            }
+        });
+    });
+
+    places.forEach(image => {
+    observer.observe(image);
+    });
+})
+
+
+    
+
+
+
+
+
